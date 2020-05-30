ea1c51d4cfcffad0698c45cc32808b516bf89536
HiroHara-Nextremer <hiroaki.hara@nextremer.com>
2020-05-30 20:00:42 +0900

    Add function to create symlinks

--- etc/init.sh
+++ etc/init.sh
@@ -2,19 +2,57 @@
 #
 # Automatically unpack dotfiles from github repository in your local $HOME directory
 
-#set -o errexit
-set -o nounset
+set -o errexit
+#set -o nounset
 set -o pipefail
 
 ##################################################################
 # Helper functions
 ##################################################################
+logging() {
+
+  :
+
+}
+
+logger_info() {
+
+  :
+
+}
+
+logger_error() {
+
+  :
+
+}
+
 has() {
 
   which "$1" >/dev/null 2>&1
 
 
 }
+
+
+##################################################################
+# Create symbolic links of the cloned dotfiles
+# Globals(used and modified):
+#   used: DOTFILES_PATH, GITHUB_REPO_URL
+# Arguments:
+#   None
+# Returns(other then default exit status of the last command run):
+#   None
+##################################################################
+initialize_dotfile() {
+
+  for dot_file in "$DOTFILES_PATH"/.??*; do
+    [[ "$dot_file" == "${DOTFILES_PATH}/.git" ]] && continue
+    ln -sfnv "$dot_file" "$HOME" || exit
+  done
+
+}
+
 
 ##################################################################
 # Main script
@@ -29,8 +67,9 @@ main() {
 
   if has "git"; then
     # For test
-    git clone -b test "$GITHUB_REPO_URL" || exit 0
-    # git clone "$GITHUB_REPO_URL" || exit 0
+    git clone -b test "$GITHUB_REPO_URL" || exit
+    # git clone "$GITHUB_REPO_URL" || exit
+    initialize_dotfile
   elif has "curl" || has "wget"; then
     if has "curl"; then
       :
@@ -41,28 +80,24 @@ main() {
 
 }
 
+readonly REPO_NAME='dotfiles'
+readonly DOTFILES_PATH="$HOME/${REPO_NAME}"
+readonly GITHUB_REPO_URL="https://github.com/Himalayan-wildcat/${REPO_NAME}.git"
 
-DOTFILES_PATH="$HOME/.dotfiles"
-GITHUB_REPO_URL='https://github.com/Himalayan-wildcat/dotfiles.git'
+# TODO:
+## add if condition based on OS
+## add if condition based on shell(bash)
 
-#cd "$(dirname "$0")" || exit 0
-cd "$HOME" || exit 0
+cd "$HOME" || exit
 if [[ -d "$DOTFILES_PATH" ]]; then
-  while :; do
-    read -rp 'Dotfiles already exist. Do you want to overwrite the directory? [y/n]' input
-    case "$input" in
-      [yY]*) main
-	     break
-	     ;;
-      [nN]*) echo 'Aborted!!'
-	     exit 0
-	     ;;
-      *) echo "Aborted!! Invalid value. Enter [y/n]."
-	 clear
-	 continue
-	 ;;
-    esac
-  done
+  # Terminate the script if the dotfiles dir already exists.
+  # The dir should NOT be overwritten by executing 'rm -rf $HOME/dotfiles', which can
+  # inadvertently execute 'rm -rf $HOME'(it happnes really ... :<)
+
+  # TODO:
+  # To replace echo with logging function
+  echo 'Dotfiles directory already exist. Retry again after deleging it.'
+  exit 0
 else
   main
 fi
