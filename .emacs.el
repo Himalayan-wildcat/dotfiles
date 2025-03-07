a135b1255a93a02d65ec1b12352ac85905f57687
Hiroaki Hara(@Himalayan-wildcat) <npng.h.0709@gmail.com>
2025-03-07 20:06:02 +0900

    Implement tree-sitter

--- .emacs.el
+++ .emacs.el
@@ -11,7 +11,6 @@
 ;; ===================================================
 ;; Packages
 ;; ===================================================
-;; Create user-defined file so custom set variable/face are written to it.
 (setq custom-file (expand-file-name "~/.emacs.d/custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
   (load custom-file))
@@ -20,11 +19,37 @@
 (add-to-list 'package-archives
 	     '("gnu" . "https://elpa.gnu.org/packages/") t)
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar package-list
+  '(magit
+    jsonrpc
+    smartparens
+    rainbow-delimiters
+    highlight-indent-guides
+    vertico
+    consult
+    flymake-diagnostic-at-point
+    flymake-ruff
+    reformatter
+    company
+    yaml-mode
+    dockerfile-mode
+    docker
+    docker-tramp
+    rust-mode
+    python-mode)
+)
+
+(dolist (p package-list)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
 ;; straight.el
-;; https://github.com/radian-software/straight.el?tab=readme-ov-file#getting-started
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name
@@ -41,42 +66,6 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-;; Load the latest version of th all packages, making then available for download.
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
-;; The packages to be installed.
-(defvar package-list
-  '(
-    vertico
-    consult
-    flymake-diagnostic-at-point
-    flymake-ruff
-    company
-    magit
-    python-mode
-    json-mode
-    yaml-mode
-    terraform-mode
-    dockerfile-mode
-    js2-mode
-    go-mode
-    rust-mode
-    markdown-mode
-    reformatter
-    highlight-indent-guides
-    tree-sitter
-    lsp-mode
-    lsp-ui
-    lsp-pyright
-    editorconfig
-    jsonrpc)
- )
-
-;; Install them if the packages in 'package-list' are yet installed.
-(dolist (p package-list)
-  (when (not (package-installed-p p))
-    (package-install p)))
 
 ;; ===================================================
 ;; Global config
@@ -86,6 +75,11 @@
 ;; cf. https://emacsWiki.org/emacs/BackspaceKey
 (define-key key-translation-map [?\C-h] [?\C-?])
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(global-set-key (kbd "C-M-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "C-M-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "C-M-<up>") 'shrink-window)
+(global-set-key (kbd "C-M-<down>") 'enlarge-window)
+
 
 (when (eq system-type 'darwin)
   (define-key global-map [?\M-¥] [?\\]))
@@ -119,7 +113,8 @@
 ;; Major mode
 (add-hook 'emacs-lisp-mode-hook (lambda()
 				  (show-paren-mode 1)
-				  (setq show-paren-delay 0)))
+                                  (setq show-paren-delay 0))
+)
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
@@ -145,9 +140,6 @@
     (interactive)
     (call-process-region (point) (if mark-active (mark) (point)) "pbpaste" t t)))
 
-;;(setq mac-command-key-is-meta nil)
-;;(setq mac-option-modifier 'meta)
-;;(setq mac-command-modifier 'super)
 (defun line-on()
   "Show line number."
   (interactive)
@@ -171,28 +163,71 @@
 ;; =========================================
 ;; Settings by packages
 ;; =========================================
-;; vertico
+(use-package magit
+  :ensure t
+  :bind
+  ("C-x g" . magit-status)
+)
+
 (use-package vertico
   :ensure t
   :init
-  (vertico-mode))
+  (vertico-mode)
+)
 
-;; flymake
+(use-package smartparens
+  :ensure t
+  :hook
+  (prog-mode text-mode markdown-mode)
+  (after-init-hook . smartparents-global-strict-mode)
+  :config
+  (require 'smartparens-config)
+)
+
+(use-package rainbow-delimiters
+  :ensure t
+  :hook
+  (prog-mode . rainbow-delimiters-mode)
+)
+
+;; (use-package highlight-indent-guides
+;;   :ensure t
+;;   :delight
+;;   :custom
+;;   (highlight-indent-guides-method  'character)
+;;   (highlight-indent-guides-auto-enabled t)
+;;   (highlight-indent-guides-responsive t)
+;;   (highlight-indent-guides-character ?|)
+;;   (set-face-background 'highlight-indent-guides-odd-face "darkgray")
+;;   (set-face-background 'highlight-indent-guides-even-face "dimgray")
+;;   (set-face-foreground 'highlight-indent-guides-character-face "dimgray")
+;;   :hook
+;;   ((python-mode yaml-mode) . highlight-indent-guides-mode)
+;; )
+
 (use-package flymake
   :ensure t
-  :bind (nil
-         :map flymake-mode-map
-         ("C-c C-p" . flymake-goto-prev-error)
-         ("C-c C-n" . flymake-goto-next-error))
+  :bind
+  (nil :map flymake-mode-map
+       ("C-c C-p" . flymake-goto-prev-error)
+       ("C-c C-n" . flymake-goto-next-error))
   :config
-  (set-face-background 'flymake-errline "gray")
-  (set-face-background 'flymake-warnline "DarkOrange")
+  (custom-set-faces
+    '(flymake-errline
+       ((((class color))
+       (:foreground "Red" :bold t :underline t :background "lemon chiffon"))))
+    '(flymake-warnline
+       ((((class color))
+     (:foreground "Red" :bold t :underline t :background "LimeGreen")))))
+  ;;
+  ;; (set-face-attribute 'flymake-warning nil
+  ;; 		      :underline '(:style wave :color "orange"))
+  ;; (set-face-attribute 'flymake-errline nil
+  ;; 		      :underline '(:style wave :color "red"))
+  ;; (set-face-background 'flymake-errline "gray")
+  ;; (set-face-background 'flymake-warnline "darkgray")
+  ;; (set-face-background 'flymake-warnline "DarkOrange")
 )
-;;   :config
-;; (custom-set-faces
-;;  '(flymake-errline ((((class color)) (:underline "red"))))
-;;  '(flymake-warnline ((((class color)) (:underline "yellow")))))
-
 
 (use-package flymake-diagnostic-at-point
   :ensure t
@@ -200,66 +235,72 @@
   :config
   (remove-hook 'flymake-diagnostic-functions 'flymake-proc-legacy-flymake)
   :hook
-  (flymake-mode . flymake-diagnostic-at-point-mode))
-
-;; lsp
-(defun efs/lsp-mode-setup ()
-  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
-  (lsp-headerline-breadcrumb-mode)
+  (flymake-mode . flymake-diagnostic-at-point-mode)
 )
 
-(use-package lsp-mode
-  :commands (lsp lsp-deferred)
+(use-package company
+  :ensure t
+)
+
+(use-package eglot
+  :ensure t
+  :config
+  (add-to-list 'eglot-stay-out-of 'company)
   :bind
+  (:map eglot-mode-map
   ("C-x C-e" . xref-find-definitions)
   ("C-x C-d" . xref-find-definitions-other-window)
   ("C-x C-r" . xref-find-references)
-  ("C-x C-p" . xref-pop-marker-stack)
-  :hook (lsp-mode . efs/lsp-mode-setup)
-)
-
-(use-package lsp-ui
-  :after lsp-mode
-  :config
-  (setq lsp-ui-peek-enable t)
-  (setq lsp-ui-peek-always-show t)
-  (setq lsp-ui-sideline-show-diagnostics t)
-  (setq lsp-ui-sideline-show-code-actions t)
-  ;; ホバーで表示されるものを、ホバーの変わりにsidelineで表示する
-  ;; (setq lsp-ui-sideline-show-hover t)
-  :bind
-  (:map lsp-ui-mode-map
-        ;; デフォルトの xref-find-definitions だと、ジャンプはできるが、ui-peek が使えない。
-        ("M-." . lsp-ui-peek-find-definitions)
-
-        ;; デフォルトの xref-find-references だと、ジャンプはできるが、ui-peek が使えない。
-        ("M-?" . lsp-ui-peek-find-references)
-        )
+  ("C-x C-p" . xref-pop-marker-stack))
   :hook
-  (lsp-mode . lsp-ui-mode)
+  (eglot--managed-mode . company-mode)
 )
 
-(use-package highlight-indent-guides
+(use-package copilot
+  :straight (:host github :repo "copilot-emacs/copilot.el" :files ("*.el"))
   :ensure t
-  :delight
   :hook
-  (python-mode . highlight-indent-guides-mode)
-  ;; (prog-mode . highlight-indent-guides-mode)
-  :custom
-  (highlight-indent-guides-method 'fill)
-  (highlight-indent-guides-auto-enabled t)
-  (highlight-indent-guides-responsive t)
-  (highlight-indent-guides-character ?\|)
-  (set-face-foreground 'highlight-indent-guides-character-face 'dimgray)
+  (prog-mode . copilot-mode)
+  :bind
+  (:map copilot-completion-map
+	("<tab>" . 'copilot-accept-completion)
+        ("TAB" . 'copilot-accept-completion)
+        ("C-TAB" . 'copilot-accept-completion-by-word)
+        ("C-<tab>" . 'copilot-accept-completion-by-word))
+)
+
+(use-package rust-mode
+  :ensure t
+  :mode ("\\.rs\\'" . rust-mode)
+  :init
+  (setq rust-mode-treesitter-derive t)
+  :config
+  (setq rust-format-on-save t)
+  :hook
+  (rust-mode . eglot-ensure)
 )
 
 ;; Python
-;; cf.)
-;; - https://github.com/mkt3/ruff-fix.el
-;; - https://mako-note.com/ja/python-emacs-eglot/#ruff
-;; - https://tam5917.hatenablog.com/entry/2024/07/01/150557
-;; - https://mako-note.com/ja/emacs-ruff/
-;; - https://blog.symdon.info/posts/1602395756/#headline-18
+(use-package flymake-ruff
+  :ensure t
+  :after flymake
+  :hook
+  (python-mode . flymake-ruff-load)
+)
+
+(use-package reformatter
+  :ensure t
+  :hook
+  (python-mode . ruff-format-on-save-mode)
+  :config
+  (reformatter-define ruff-format
+    :program "ruff"
+    :args `("format" "--stdin-filename", buffer-file-name "-"))
+  (reformatter-define ruff-sort-imports
+    :program "ruff"
+    :args `()
+)
+
 (defun ruff-fix-buffer ()
   "Use ruff to fix lint violations in the current buffer."
   (interactive)
@@ -277,7 +318,7 @@
     (insert-file-contents temp-file)
     (delete-file temp-file)
     (goto-char current-point)))
-
+)
 
 (defun ruff-fix-before-save ()
   (interactive)
@@ -286,213 +327,72 @@
 
 (use-package python-mode
   :ensure t
-  :mode("\\.py\\'" . python-mode)
-  ;; :init
-  ;; (add-hook 'before-save-hook 'ruff-fix-before-save)
+  :mode ("\\.py\\'" . python-mode)
   :hook
-  (python-mode . lsp-deferred)
-  (python-mode . display-fill-column-indicator-mode)
   (python-mode . python-ts-mode)
+  (python-mode . display-fill-column-indicator-mode)
+  (python-mode . auto-fill-mode)
   (python-mode . (lambda()
-		   (setq-default display-fill-column-indicator-column 88)
-		   (setq display-fill-column-indicator t)))
+		   (setq fill-column 79)))
+  (python-mode . eglot-ensure)
   (before-save . ruff-fix-before-save)
 )
 
-(use-package lsp-pyright
+(use-package typescript-ts-mode
   :ensure t
-  :custom (lsp-pyright-langserver-command "pyright")
-  :hook (python-mode . (lambda ()
-                          (require 'lsp-pyright)
-                          (lsp-deferred)))
-)
-
-(use-package flymake-ruff
-  :ensure t
-  :hook (python-mode . flymake-ruff-load))
-
-(use-package reformatter
-  :ensure t
-  :hook
-  (python-ts-mode . ruff-format-on-save-mode)
+  :mode
+  ("\\.ts\\'" . tsx-ts-mode)
+  ("\\.tsx\\'" . tsx-ts-mode)
+  ("\\.mts\\'" . tsx-ts-mode)
+  ("\\.js\\'" . tsx-ts-mode)
+  ("\\.jsx\\'" . tsx-ts-mode)
+  ("\\.mjs\\'" . tsx-ts-mode)
+  ("\\.cjs\\'" . tsx-ts-mode)
   :config
-  (reformatter-define ruff-format
-    :program "ruff"
-    :args `("format" "--stdin-filename" ,buffer-file-name "-")))
-
-;; Golang
-(defun lsp-go-before-save-hooks()
-  (add-hook 'before-save-hook #'lsp-format-buffer t t)
-  (add-hook 'before-save-hook #'lsp-organize-imports t t)
-)
-
-(defun go-fmt()
-  (setq gofmt-command "goimports")
-  (add-hook 'before-save-hook #'gofmt-before-save t t)
-)
-
-(defun go-tab-width()
-  (setq indent-tabs-mode nil)
-  (setq tab-width 8)
-)
-
-(use-package go-mode
-  :ensure t
-  :mode ("\\.go\\'" . go-mode)
+  (setq typescript-ts-mode-indent-offset 2)
   :hook
-  (go-mode . lsp-deferred)
-  (go-mode . go-tab-width)
-  (go-mode . go-fmt)
-)
-  ;; (go-mode . lsp-go-before-save-hooks))
-
-;; Terraform
-(use-package terraform-mode
-  :ensure t
-  :mode ("\\.tf\\'" . terraform-mode)
-  :hook
-  (terraform-mode . lsp-deferred)
-  (terraform-mode . terraform-format-on-save-mode)
+  (typescript-ts-mode . eglot-ensure)
+  (tsx-ts-mode . eglot-ensure)
 )
 
-;; Copilot
-;; cf.) https://github.com/copilot-emacs/copilot.el
-(use-package copilot
-  :straight (:host github :repo "copilot-emacs/copilot.el" :files ("*.el"))
-  :ensure t
-  :hook (prog-mode . copilot-mode)
-  :bind (:map copilot-completion-map
-             ("<tab>" . 'copilot-accept-completion)
-             ("TAB" . 'copilot-accept-completion)
-             ("C-TAB" . 'copilot-accept-completion-by-word)
-             ("C-<tab>" . 'copilot-accept-completion-by-word)
-	)
-)
-
-;; ;; lsp
-;; (use-package lsp-mode
-;;   :ensure t
-;;   :commands (lsp lsp-deferred)
-;;   :after (:any go-mode rust-mode terraform-mode)
-;;   :bind
-;;   (("C-x C-e" . xref-find-definitions)
-;;    ("C-x C-d" . xref-find-definitions-other-window)
-;;    ("C-x C-r" . xref-find-references)
-;;    ("C-x C-p" . xref-pop-marker-stack))
-;;   :hook
-;;   ;; For go-mode
-;;   ((go-mode . lsp-deferred)
-;;    (go-mode . lsp-go-before-save-hooks)
-
-
-;;   ;; For rust-mode
-;;   (rust-mode . lsp)
-;;   ;; For terraform-mode
-;;   (terraform-mode . lsp-deferred))
-;;   :custom
-;;   (lsp-rust-server 'rust-analyzer)
-;;   (lsp-enable-snippet nil))
-;;   :config
-;;   (with-eval-after-load 'lsp-mode
-;;     (lsp-register-client
-;;       (make-lsp-client :new-connection (lsp-stdio-connection '("~/.go/bin/terraform-lsp" "serve"))
-;;                        :major-modes '(terraform-mode)
-;;                        :server-id 'terraform-ls)))
-
-
-;;   :ensure t
-;;   :mode("\\.py\\'" . python-mode)
-;;   :init
-;;   (setq-default indent-tabs-mode nil)
-;;   :config
-;;   (setq python-indent-offset 4)
-;;   :hook
-;;   ((python-mode . smartparens-mode)
-;;    (python-mode . color-identifiers-mode))
-
-
-;; Rust
-(when (eq system-type 'darwin)
-  (add-to-list 'exec-path (expand-file-name "/usr/local/bin/rust-analyzer")))
-
-(when (eq system-type 'gnu/linux)
-  (add-to-list 'exec-path (expand-file-name "/usr/local/bin/rust-analyzer")))
-
-(use-package rust-mode
-  :ensure t
-  :mode ("\\.rs\\'" . rust-mode)
-  :bind (:map rust-mode-map
-    ("C-c C-r" . 'rust-run)
-    ("C-c C-t" . 'rust-test)
-    ("C-c C-c" . 'rust-compile)
-    ("C-c C-e" . 'rust-check))
-  :hook
-  (rust-mode . lsp-deferred)
-  (rust-mode . (lambda()
-    (setq indent-tabs-mode nil)
-    (setq rust-format-on-save t)
-    (prettify-symbols-mode))
-  )
-)
-
-;; Json
-(use-package json-mode
-  :ensure t
-  :mode("\\.json\\'" . json-mode)
+(use-package treesit
   :config
-  (setq json-reformat:indent-width 2)
+  (setq treesit-font-lock-level 4)
 )
 
-;; Markdown
-(use-package markdown-mode
+;; (use-package tree-sitter
+;;   :ensure t
+;;   :hook
+;;   (typescript-ts-mode . tree-sitter-hl-mode)
+;;   (tsx-ts-mode . tree-sitter-hl-mode)
+;;   :config
+;;   (global-tree-sitter-mode)
+;; )
+
+(use-package tree-sitter-langs
   :ensure t
-  :commands (markdown-mode gfm-mode)
-  :mode (("README\\.md\\'" . gfm-mode)
-	 ("\\.md\\'" . markdown-mode))
-  :init
-  (setq markdown-command "multimarkdown")
-  (setq-default indicate-empty-lines t)
+  ;; :after tree-sitter
+  :config
+  (tree-sitter-require 'tsx)
+  (add-to-list 'tree-sitter-major-mode-language-alist '(tsx-ts-mode . tsx))
+  (tree-sitter-require 'rust)
+  (add-to-list 'tree-sitter-major-mode-language-alist '(rust-ts-mode . rust))
 )
 
-;; Magit
-(use-package magit
+(use-package dockerfile-mode
   :ensure t
-  :bind ("C-x g" . magit-status)
+  :mode ("Dockerfile\\'" . dockerfile-mode)
 )
 
-;; Yaml
+(use-package docker
+  :ensure t
+  :bind
+  ("C-c d" . docker)
+)
+
 (use-package yaml-mode
   :ensure t
-  :mode (("\\.yaml\\'" . yaml-mode)
-         ("\\.yml\\'" . yaml-mode)))
-
-;; Docker
-(use-package dockerfile-mode
-  :commands (dockerfile-mode dockerfile-mode)
-  :mode ("Dockerfile\\'" . dockerfile-mode))
-
-;; JS
-(use-package js2-mode
-  :ensure t
-  :mode("\\.js\\'" . js2-mode)
-  :hook
-  (js2-mode . (lambda()
-    (setq indent-tabs-mode nil)
-    (setq js2-basic-offset 2)
-    (setq tab-width 2))))
-
-;; typescript
-;;(require 'typescript-mode)
-;;(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
-;;(add-hook 'typescript-mode-hook
-;;          (lambda ()
-;;            (tide-setup)
-;;            (flycheck-mode t)
-;;            (setq flycheck-check-syntax-automatically '(save mode-enabled))
-;;            (eldoc-mode t)
-;;            (company-mode-on)))
-
-;;(profiler-report)
-;;(profiler-stop)
+  :mode ("\\.yml\\'" . yaml-mode)
+)
 
 ;;; .emacs.el ends here
